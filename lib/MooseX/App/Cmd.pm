e1a5e109816aadc541473c85185cab04b40d38e9
Yuval Kogman <nothingmuch@woobling.org>
2008-01-16 00:22:52 +0000

    doc and releng

--- lib/MooseX/App/Cmd.pm
+++ lib/MooseX/App/Cmd.pm
@@ -13,7 +13,7 @@ __END__
 
 =head1 NAME
 
-MooseX::App::Cmd - Combines L<MooseX::Getopt> with L<App::Cmd>.
+MooseX::App::Cmd - Mashes up L<MooseX::Getopt> and L<App::Cmd>.
 
 =head1 SYNOPSIS
 
@@ -22,12 +22,14 @@ See L<App::Cmd/SYNOPSIS>.
     package YourApp::Cmd;
 	use Moose;
 
-    extends qw/MooseX::App::Cmd/;
+    extends qw(MooseX::App::Cmd);
+
+
 
     package YourApp::Cmd::Command::blort;
     use Moose;
 
-    extends qw/MooseX::App::Cmd::Command/;
+    extends qw(MooseX::App::Cmd::Command);
 
     has blortex => (
         isa => "Bool",
@@ -46,7 +48,7 @@ See L<App::Cmd/SYNOPSIS>.
     sub run {
         my ( $self, $opt, $args ) = @_;
 
-        # you may ignore $opt, it's in the fields
+        # you may ignore $opt, it's in the attributes anyway
         
         my $result = $self->blortex ? blortex() : blort();
 
@@ -57,6 +59,23 @@ See L<App::Cmd/SYNOPSIS>.
 
 =head1 DESCRIPTION
 
+This module marries L<App::Cmd> with L<MooseX::Getopt>.
+
+Use it like L<App::Cmd> advises (especially see L<App::Cmd::Tutorial>),
+swapping L<App::Cmd::Command> for L<MooseX::App::Cmd::Command>.
+
+Then you can write your moose commands as moose classes, with L<MooseX::Getopt>
+defining the options for you instead of C<opt_spec> returning a
+L<Getopt::Long::Descriptive> spec.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT
+
+    Copyright (c) 2007-2008 Infinity Interactive, Yuval Kogman. All rights
+    reserved This program is free software; you can redistribute it and/or
+    modify it under the same terms as Perl itself.
+
 =cut
-
-
--- lib/MooseX/App/Cmd/Command.pm
+++ lib/MooseX/App/Cmd/Command.pm
@@ -7,6 +7,8 @@ with qw/MooseX::Getopt/;
 
 extends qw(Moose::Object App::Cmd::Command);
 
+with qw(MooseX::Getopt);
+
 use Getopt::Long::Descriptive ();
 
 sub _process_args {
@@ -14,9 +16,6 @@ sub _process_args {
 
     # @params is ignored here. it's the null list returned by opt_spec, which _attrs_to_options doesn't use
     # we're passing it around for future use if any.
-
-    #if ( $class->does("MooseX::Getopt") ) {
-    # presumably there are many styles of getopt
 
     local @ARGV = @$args;
 
@@ -31,7 +30,6 @@ sub _process_args {
 
         %{ $processed{params} }, # params from CLI are also fields in MooseX::Getopt
     );
-    #}
 }
 
 __PACKAGE__;
@@ -65,6 +63,29 @@ MooseX::App::Cmd::Command - Base class for L<MooseX::Getopt> based L<App::Cmd::C
     }
 
 =head1 DESCRIPTION
+
+This is a replacement base class for L<App::Cmd::Command> classes that includes
+L<MooseX::Getopt> and the glue to combine the two.
+
+=head1 METHODS
+
+=over 4
+
+=item _process_args
+
+Replaces L<App::Cmd::Command>'s argument processing in in favour of
+L<MooseX::Getopt> based processing.
+
+=back
+
+=head1 TODO
+
+Full support for L<Getopt::Long::Descriptive>'s abilities is not yet written.
+
+This entails taking apart the attributes and getting at the descriptions.
+
+This might actually be added upstream to L<MooseX::Getopt>, so until we decide
+here's a functional but not very helpful (to the user) version anyway.
 
 =cut
 
